bf8af6a55bc217e696baef896ab574aaacb7042a
niloynkj <66770175+niloynkj@users.noreply.github.com>
2021-03-12 02:37:22 +0600

    Update Survay.js

--- Survay.js
+++ Survay.js
@@ -1,3 +1,13 @@
+var timeleft = 10;
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+	document.getElementById("btn").disabled = false;
+  }
+  document.getElementById("progressBar").value = 10 - timeleft;
+  timeleft -= 1;
+}, 1000);
+
 var str = (window.location != window.parent.location)
             ? document.referrer
             : document.location.href;
